ed0a9ae0d6e7ffec39bb3c2ffa552c1fb5e4b4bf
Neviim <jorge@fcn.edu.br>
2024-03-04 16:46:34 -0300

    relay

---
+++ src/assinando_relay.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import uuid
+
+import tornado.ioloop
+from rich.console import Console
+from rich.table import Table
+from tornado import gen
+
+from pynostr.base_relay import RelayPolicy
+from pynostr.event import EventKind
+from pynostr.filters import Filters, FiltersList
+from pynostr.message_pool import MessagePool
+from pynostr.relay import Relay
+from pynostr.utils import get_public_key
+
+if __name__ == "__main__":
+
+    console = Console()
+
+    # input_str = input("author (npub or nip05): ")
+    input_str = "npub1g94kghlmds4j3329ve3vv773tnyl8nr89fqta30jwntcym2sty0sf4l9sp" #Qubit
+    recipient = ""
+    author = get_public_key(input_str)
+
+    relay_url = input("relay (wss://...): ")
+    relay_url = "wss://"+relay_url
+
+    filters = FiltersList(
+        [Filters(authors=[author.hex()], kinds=[EventKind.TEXT_NOTE], limit=100)]
+    )
+
+    subscription_id = uuid.uuid1().hex
+    io_loop = tornado.ioloop.IOLoop.current()
+    message_pool = MessagePool(first_response_only=False)
+    policy = RelayPolicy()
+    r = Relay(relay_url, message_pool, io_loop, policy, timeout=3)
+
+    r.add_subscription(subscription_id, filters)
+
+    try:
+        io_loop.run_sync(r.connect)
+    except gen.Return:
+        pass
+    io_loop.stop()
+
+    event_msgs = message_pool.get_all_events()
+    print(f"{r.url} returned {len(event_msgs)} TEXT_NOTEs from {input_str}.")
+
+    table = Table("date", "content")
+    for event_msg in event_msgs[::-1]:
+        table.add_row(str(event_msg.event.date_time()), event_msg.event.content)
+    console.print(table)
